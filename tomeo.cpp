3b4c909621c388c9c3b988b9381ee4974383b20c
Alex Cheong <sc20a2c@leeds.ac.uk>
2021-11-30 13:26:04 +0000

    Revert "Merge branch 'browse-button' into 'main'"
    This reverts merge request !3

--- tomeo.cpp
+++ tomeo.cpp
@@ -17,9 +17,7 @@
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QHBoxLayout>
 #include <QScrollArea>
-#include <QWidget>
 #include <QSlider>
-#include <QMessageBox>
 #include <QtCore/QFileInfo>
 #include <QtWidgets/QFileIconProvider>
 #include <QDesktopServices>
@@ -153,8 +151,7 @@ int main(int argc, char *argv[]) {
     //create scrollArea for the thumbnail container
     QScrollArea *scrollArea = new QScrollArea();
     scrollArea->setWidget(buttonWidget);
-    scrollArea->setMaximumWidth(465);   //makes the maximum size for the thumbnail column fit 2 thumbnails with appropriate spacing
-    scrollArea->setMinimumWidth(465);
+    scrollArea->setMaximumWidth(480);   //makes the maximum size for the thumbnail column fit 2 thumbnails with appropriate spacing
 
     // create the main window and layout
     QWidget window;
@@ -164,45 +161,11 @@ int main(int argc, char *argv[]) {
     window.setWindowTitle("tomeo");
     window.setMinimumSize(800, 680);
 
-    //creating the layout and its  widget
-    QWidget *browseButtonsWidget = new QWidget();
-    QHBoxLayout *browseButtonLayout = new QHBoxLayout();
-    browseButtonsWidget->setLayout(browseButtonLayout); // set layout
-
-
-    //create add video button & set its size
-    QPushButton *addVideo = new QPushButton();
-    addVideo->setText("Add video...");
-    addVideo->setFixedSize(200, 30);
-    // create message box
-    QMessageBox *addVideoMessage = new QMessageBox();
-    addVideoMessage->setWindowTitle("Add Video...");
-    addVideoMessage->setText("Congratulations, you've added new a new video");
-    // connect add video button to message box
-    QObject::connect(addVideo, SIGNAL(clicked()), addVideoMessage, SLOT(exec()));
-
-
-    //create import folder button & set its size
-    QPushButton *importFolder = new QPushButton();
-    importFolder->setText("Import folder...");
-    importFolder->setFixedSize(200, 30);
-    // create message box
-    QMessageBox *importFolderMessage = new QMessageBox();
-    importFolderMessage->setWindowTitle("Import folder...");
-    importFolderMessage->setText("Congratulations, you've imported a new folder");
-    QObject::connect(importFolder, SIGNAL(clicked()), importFolderMessage, SLOT(exec()));
-
-    //adding widgets to layout
-    browseButtonLayout->addWidget(addVideo);
-    browseButtonLayout->addWidget(importFolder);
-
-
     // add the video and the buttons to the top level QGridLayout
     //the way items in the GridLayout works is you designate their Row then Column, then how many Rows they span over, then how many Cols they span over
     top->addWidget(playerWidget, 0, 0);   //video widget is row 0 col 0
     top->setColumnStretch(0, 1);
     top->addWidget(scrollArea, 0, 1); //scollArea is row 0 col 1
-    top->addWidget(browseButtonsWidget, 1,1);
     top->setColumnStretch(1, 1);
 
     // showtime!
